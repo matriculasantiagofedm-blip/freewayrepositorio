0bd8c2733b397a7e79797b30dc72ef06d94345ee
Freeway Escuela de Manejo Santiago <matriculasantiago.fedm@gmail.com>
2025-12-19 21:22:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-127944656-2105f",
+  "appId": "1:204502463634:web:301eace930acb20da9624b",
+  "apiKey": "AIzaSyAbxz0NBhlUwiseTrGXyBMWKyF6iB1XZNM",
+  "authDomain": "studio-127944656-2105f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "204502463634"
+};

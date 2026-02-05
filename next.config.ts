6e69bf5a74c5f6443e2fe2e6ae4b9c87d2afebbd
Freeway Escuela de Manejo Santiago <matriculasantiago.fedm@gmail.com>
2026-02-05 00:44:44 +0000

    crees que algunos de estos puntos que me muestras esten causando el prob

--- next.config.ts
+++ next.config.ts
@@ -29,14 +29,6 @@ const nextConfig: NextConfig = {
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     contentDispositionType: 'inline',
   },
-  async rewrites() {
-    return [
-      {
-        source: '/print-receipt',
-        destination: '/print-receipt',
-      },
-    ]
-  },
 };
 
 export default nextConfig;

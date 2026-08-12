28a0a28ba4f7a8b65697bab34f488605a2e01b03
ContractTime Dev <dev@contractimefedm.com>
2026-08-11 19:30:52 -0500

    Forzar invalidacion de cache para nuevo menu Entrega de Certificados

--- next.config.js
+++ next.config.js
@@ -4,6 +4,18 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate, max-age=0' },
+          { key: 'CDN-Cache-Control', value: 'no-store' },
+          { key: 'Vercel-CDN-Cache-Control', value: 'no-store' },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
